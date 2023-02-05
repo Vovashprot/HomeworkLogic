8223da9d32506a5f9fb12bbf3ad9acb4bd647f60
Vovashprot <vova.protasenko@gmail.com>
2023-02-05 12:01:35 +0300

    2 task

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        task2();
     }
     public static void task1 () {
+        System.out.println("Task 1");
         byte age = 18;
         if (age >= 18) {
             System.out.println("Вы достигли совершеннолетия");
@@ -11,5 +12,16 @@ public class Main {
         else {
             System.out.println("Вы ещё не достигли совершеннолетия");
         }
+    }
+
+    public static void task2 () {
+        System.out.println("Task 2");
+      byte temp = 3;
+      if (temp >= 5) {
+          System.out.println("Сегодня тепло, можно идти без шапки");
+      }
+      else {
+          System.out.println("На улице холодно, нужно надеть шапку");
+      }
     }
 }
