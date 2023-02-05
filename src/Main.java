aafd95f9df7bb4cb4db96dd8678ee3492caae61c
Vovashprot <vova.protasenko@gmail.com>
2023-02-05 11:50:30 +0300

    1 task

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    public static void task1 () {
+        byte age = 18;
+        if (age >= 18) {
+            System.out.println("Вы достигли совершеннолетия");
+        }
+        else {
+            System.out.println("Вы ещё не достигли совершеннолетия");
+        }
+    }
+}
